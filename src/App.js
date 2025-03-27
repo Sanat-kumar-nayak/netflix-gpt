e4b9c71eb6520479492a0bd2e96bb904782a7784
Sanat kumar nayak <sanatnayak.1997@gmail.com>
2025-03-28 00:28:07 +0530

    tailwind CSS configured

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">This is NetFlix clone</div>;
+  return <div className="font-bold">This is NetFlix clone</div>;
 }
 
 export default App;
